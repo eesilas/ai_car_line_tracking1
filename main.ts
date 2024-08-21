4004aa7beec0f8f54a0bfc3329f6d1344d891100
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2024-08-21 16:36:28 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+huskylens.initI2c()
+let x1 = 0
+let x2 = 0
+let detmine = 0
+basic.forever(function () {
+    huskylens.initMode(protocolAlgorithm.ALGORITHM_LINE_TRACKING)
+    while (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultArrow)) {
+        x2 = huskylens.readeArrow(1, Content2.xTarget)
+        x1 += huskylens.readeArrow(1, Content2.xOrigin)
+        detmine += x2 - x1
+    }
+})
